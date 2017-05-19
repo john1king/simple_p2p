7591974d9ca60556c4e2a58b2f306ba0bed2d07f
john1king <git@john1king.com>
2017-05-19 09:51:12 +0800

    Basic p2p grape api

---
+++ app/controllers/api/p2p.rb
@@ -0,0 +1,66 @@
+class API::P2p < Grape::API
+  version 'v1', using: :path
+  format :json
+  prefix :api
+
+  resource :users do
+    desc 'create user'
+    params do
+      requires :name, type: String, desc: 'User name'
+      optional :money, type: BigDecimal, desc: 'User money'
+    end
+    post do
+      User.create!(name: params[:name], money: params[:money].presence || 0)
+    end
+
+    desc 'get user balances'
+    get ':id/balances' do
+      user = User.find(params[:id])
+      {
+        id: user.id,
+        moeny: user.money,
+        borrow_money: user.amount_borrow_money,
+        lend_money: user.amount_lend_money,
+      }
+    end
+
+    desc 'get balances between two users'
+    params do
+      requires :from, type: Integer, desc: 'User id who borrow money from other'
+      requires :to, type: Integer, desc: 'User id who lend moeny to other'
+    end
+    get 'balances' do
+      {
+        money: User.find(params[:from]).money_borrow_from(User.find(params[:to]))
+      }
+    end
+
+  end
+
+  resource :borrow_tradings do
+    desc 'create a borrow trading'
+    params do
+      requires :from, type: Integer, desc: 'User id who borrow money from other'
+      requires :to, type: Integer, desc: 'User id who borrow moeny to other'
+      requires :money, type: BigDecimal, desc: 'Number of borrow money'
+    end
+    post do
+      User.find(params[:from]).borrow_from(User.find(params[:to]), params[:money])
+      {}
+    end
+  end
+
+  resource :refund_tradings do
+    desc 'create a refund trading'
+    params do
+      requires :from, type: Integer, desc: 'User id who refund money to other'
+      requires :to, type: Integer, desc: 'User id who lend moeny to other'
+      requires :money, type: BigDecimal, desc: 'Number of borrow money'
+    end
+    post do
+      User.find(params[:from]).refund_to(User.find(params[:to]), params[:money])
+      {}
+    end
+  end
+
+end
---
+++ test/api/p2p_test.rb
@@ -0,0 +1,75 @@
+require 'test_helper'
+
+class API::P2pTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+
+  def app
+    API::P2p
+  end
+
+  def json_response
+    JSON.parse(last_response.body)
+  end
+
+  test 'should create user' do
+    post '/api/v1/users', { name: 'a', money: 100}
+    assert last_response.created?
+    assert_kind_of Integer, json_response['id']
+  end
+
+  test 'should create borrow tradings and refund tradings' do
+    post '/api/v1/users', { name: 'a', money: 0}
+    from = json_response['id']
+    post '/api/v1/users', { name: 'b', money: 100}
+    to = json_response['id']
+    post '/api/v1/borrow_tradings', { from: from, to: to, money: 70 }
+    assert_equal 201, last_response.status
+
+    get "/api/v1/users/#{from}/balances"
+    assert_equal '70.0', json_response['moeny']
+
+    post '/api/v1/refund_tradings', { from: from, to: to, money: 50 }
+
+    get "/api/v1/users/#{from}/balances"
+    assert_equal '20.0', json_response['moeny']
+  end
+
+
+  test 'should get balances of users' do
+    post '/api/v1/users', { name: 'a', money: 100}
+    a = json_response['id']
+
+    post '/api/v1/users', { name: 'b', money: 100}
+    b = json_response['id']
+
+    post '/api/v1/users', { name: 'c', money: 100}
+    c = json_response['id']
+
+    post '/api/v1/borrow_tradings', { from: a, to: b, money: 100 }
+    post '/api/v1/borrow_tradings', { from: b, to: c, money: 70 }
+    post '/api/v1/borrow_tradings', { from: c, to: a, money: 30 }
+
+    get "/api/v1/users/#{a}/balances"
+
+    assert_equal '170.0', json_response['moeny']
+    assert_equal '100.0', json_response['borrow_money']
+    assert_equal '30.0', json_response['lend_money']
+
+    get "/api/v1/users/#{b}/balances"
+    assert_equal '70.0', json_response['moeny']
+    assert_equal '70.0', json_response['borrow_money']
+    assert_equal '100.0', json_response['lend_money']
+
+    get "/api/v1/users/#{c}/balances"
+    assert_equal '60.0', json_response['moeny']
+    assert_equal '30.0', json_response['borrow_money']
+    assert_equal '70.0', json_response['lend_money']
+
+    get "/api/v1/users/balances", { from: a, to: b }
+    assert_equal '100.0', json_response['money']
+
+    get "/api/v1/users/balances", { from: c, to: b }
+    assert_equal '-70.0', json_response['money']
+  end
+
+end
